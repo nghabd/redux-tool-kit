7c51424aeaee7326813e44dbbe84b29b0c920d79
nghabd <dr_nageh@yahoo.com>
2024-02-21 04:25:09 +0300

    new repo2

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import Card from "../UI/Card";
 import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
-//
+
 const Cart = (props) => {
 	const cartItems = useSelector((state) => state.cart.items);
 
